991b40d63a859a6513a4e6f0fa5f60e59af90348
Kshitij Vadake <ktj@Kshitijs-MBP.fios-router.home>
2019-10-29 21:29:05 -0400

    cleaned code

--- bamazon_schema.sql
+++ bamazon_schema.sql
@@ -9,18 +9,18 @@ CREATE TABLE products(
 	price DECIMAL(6,2) NOT NULL,
 	stock_quantity INT(6) NOT NULL, 
 	PRIMARY KEY(item_id)
-)
+);
 
 
 INSERT INTO products(product_name, department_name, price, stock_quantity)
-VALUES ('Eggs', 'Groceries', 4.00, 100),
-('Marker', 'Office_Supplies', 1.99, 50),
-('Digital_Clock', 'Electronics', 25.50, 20),
-('Painreliever', 'Health&Wellness', 12.00, 30),
+VALUES ('Eggs', 'Groceries', 4.00, 12),
+('Marker', 'Office_Supplies', 1.99, 5),
+('Digital_Clock', 'Electronics', 25.50, 4),
+('Painreliever', 'Health&Wellness', 12.00, 10),
 ('CarryOn_Backpack', 'Travel', 50.00, 10),
 ('DSLR_Lens', 'Photography', 225.00, 4),
-('Bedsheet', 'Home_Goods', 15.00, 25),
-('Sunglasses', 'Sports', 95.49, 21),
-('Shampoo', 'Toiletries', 10.00, 30),
+('Bedsheet', 'Home_Goods', 15.00, 7),
+('Sunglasses', 'Sports', 95.49, 6),
+('Shampoo', 'Toiletries', 10.00, 8),
 ('Pillow_Case', 'Home_Goods', 5.00, 10),
 ('Desk_Lamp', 'Office_Supplies', 17.00, 1) 
